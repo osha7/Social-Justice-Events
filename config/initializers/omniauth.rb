aedbbf748333e035135fe3a9e546d0326ce15f63
osha7 <ogroetz@gmail.com>
2020-11-05 11:28:06 -0700

    remove scope from omniauth.rb file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'user:email'
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  # provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'user:email'
 end
 
